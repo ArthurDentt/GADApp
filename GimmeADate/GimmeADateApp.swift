d0ce307de3c09755ea29ae45ca9b05101f7395f1
Drew Connelly <ajconne2@ncsu.edu>
2021-02-11 00:23:28 -0500

    Initial Commit

---
+++ GimmeADate/GimmeADateApp.swift
@@ -0,0 +1,17 @@
+//
+//  GimmeADateApp.swift
+//  GimmeADate
+//
+//  Created by Drew Connelly on 2/11/21.
+//
+
+import SwiftUI
+
+@main
+struct GimmeADateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
